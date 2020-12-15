eae439b5aa89267e60fdd5c93d7996918f8cc7ed
Sergey0705 <pavrun85@mail.ru>
2020-12-15 17:20:22 +0600

    Почта через sendgrid

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const nodemailer = require('nodemailer')
+const sgMail = require('@sendgrid/mail')
 const db = require('../models/db')
 const config = require('../config/config.json')
+require('dotenv').config()
 
 router.get('/', (req, res, next) => {
   const skills = db.getSkills() || []
@@ -10,31 +11,49 @@ router.get('/', (req, res, next) => {
   res.render('pages/index', { title: 'Main page', products, skills })
 })
 
-router.post('/', (req, res, next) => {
-  // TODO: Реализовать функционал отправки письма.
-  if (!req.body.name || !req.body.email || !req.body.message) {
-    console.log(req.body.text)
-    return res.json({ msgemail: 'Все поля нужно заполнить!', status: 'Error' })
-  }
-  const transporter = nodemailer.createTransport(config.mail.smtp)
-  const mailOptions = {
-    from: `"${req.body.name}" <${req.body.email}>`,
-    to: config.mail.smtp.auth.user,
-    subject: config.mail.subject,
-    text:
-      req.body.message.trim().slice(0, 500) +
-      `\n Отправлено с: <${req.body.email}>`,
-  }
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      return res.json({
-        msgemail: `При отправке письма произошла ошибка!: ${error}`,
-        status: 'Error',
-      })
+router.post('/', async (req, res, next) => {
+  try {
+    sgMail.setApiKey(process.env.SEND_GRID_API_KEY)
+    const msg = {
+      to: config.mail.smtp.auth.user,
+      from: `"${req.body.name}" <${req.body.email}>`,
+      subject: config.mail.subject,
+      text: req.body.message.trim().slice(0, 500) +
+          `\n Отправлено с: <${req.body.email}>`,
     }
-    res.json({ msgemail: 'Письмо успешно отправлено!', status: 'Ok' })
-  })
+    await sgMail.send(msg)
+  } catch (err) {
+      return res.json({
+      msgemail: `При отправке письма произошла ошибка!: ${err}`,
+      status: 'Error',
+    })
+  }
+  res.json({ msgemail: 'Письмо успешно отправлено!', status: 'Ok' })
+  // TODO: Реализовать функционал отправки письма.
+  // if (!req.body.name || !req.body.email || !req.body.message) {
+  //   console.log(req.body.text)
+  //   return res.json({ msgemail: 'Все поля нужно заполнить!', status: 'Error' })
+  // }
+  // const transporter = nodemailer.createTransport(config.mail.smtp)
+  // const mailOptions = {
+  //   from: `"${req.body.name}" <${req.body.email}>`,
+  //   to: config.mail.smtp.auth.user,
+  //   subject: config.mail.subject,
+  //   text:
+  //     req.body.message.trim().slice(0, 500) +
+  //     `\n Отправлено с: <${req.body.email}>`,
+  // }
+  // transporter.sendMail(mailOptions, function (error, info) {
+  //   if (error) {
+  //     return res.json({
+  //       msgemail: `При отправке письма произошла ошибка!: ${error}`,
+  //       status: 'Error',
+  //     })
+  //   }
+  //   res.json({ msgemail: 'Письмо успешно отправлено!', status: 'Ok' })
+  // })
   // res.send('Реализовать функционал отправки письма')
+
 })
 
 module.exports = router
